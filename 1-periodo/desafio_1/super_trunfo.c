9a6867e85c2fbb94d5b7956d53f52826c0d9dc69
pxdronunxs <pn4334@gmail.com>
2025-12-17 18:51:09 -0300

    nivel aventureiro

--- 1-periodo/desafio_1/super_trunfo.c
+++ 1-periodo/desafio_1/super_trunfo.c
@@ -4,12 +4,15 @@
 
 int main(){
     char estado1;
-    char codigo1[4];
+    char codigo1[5];
     char cidade1[35];
     int populacao1;
     float area1;
     float pib1;
     int pontos_turisticos1;
+    float densidade1;
+    float pib_percapta1;
+
 
     printf("=== CADASTRO CARTA 1 ===\n");
 
@@ -20,7 +23,7 @@ int main(){
     scanf("%s", codigo1);
 
     printf("Nome da cidade: ");
-    scanf(" %[^\n]", cidade1);
+    scanf(" %34[^\n]", cidade1);
 
     printf("Populacao: ");
     scanf("%d", &populacao1);
@@ -34,17 +37,26 @@ int main(){
     printf("Numero de pontos turísticos: ");
     scanf("%d", &pontos_turisticos1);
 
+    // calculando densidade e pib per capita carta1
+
+    densidade1 = (float) populacao1 / area1;
+    pib_percapta1 = (float) (pib1 * 1000000000) / populacao1;
+    // PIB informado em bilhoes de reais e convertido para reais no calculo do PIB per capita
+
+
 // declarando variáveis da segunda carta
 
     char estado2;
-    char codigo2[4];
+    char codigo2[5];
     char cidade2[35];
     int populacao2;
     float area2;
     float pib2;
     int pontos_turisticos2;
+    float densidade2;
+    float pib_percapta2;
 
-    printf("=== CADASTRO CARTA 2 ===\n");
+    printf("\n=== CADASTRO CARTA 2 ===\n");
 
     printf("Estado (Letras de A a H): ");
     scanf(" %c", &estado2);
@@ -53,7 +65,7 @@ int main(){
     scanf("%s", codigo2);
 
     printf("Nome da cidade: ");
-    scanf(" %[^\n]", cidade2);
+    scanf(" %34[^\n]", cidade2);
 
     printf("Populacao: ");
     scanf("%d", &populacao2);
@@ -67,6 +79,12 @@ int main(){
     printf("Numero de pontos turísticos: ");
     scanf("%d", &pontos_turisticos2);
 
+    // calculando densidade e pib per capita carta2
+
+    densidade2 = (float) populacao2 / area2;
+    pib_percapta2 = (float) (pib2 * 1000000000) / populacao2;
+    // PIB informado em bilhoes de reais e convertido para reais no calculo do PIB per capita
+
 // imprimir os dados na tela
 
     printf("\n===CARTA 1===\n");
@@ -77,6 +95,8 @@ int main(){
     printf("Area: %.2f km²\n", area1);
     printf("PIB: %.2f bilhões de reais\n", pib1);
     printf("Numero de pontos turísticos: %d\n", pontos_turisticos1);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade1);
+    printf("PIB Per capita: R$ %.2f por pessoa\n", pib_percapta1);
 
     printf("\n===CARTA 2===\n");
     printf("Estado: %c\n", estado2);
@@ -86,6 +106,12 @@ int main(){
     printf("Area: %.2f km²\n", area2);
     printf("PIB: %.2f bilhões de reais\n", pib2);
     printf("Numero de pontos turísticos: %d\n", pontos_turisticos2);
+    printf("Densidade populacional: %.2f hab/km²\n", densidade2);
+    printf("PIB Per capita: R$ %.2f por pessoa\n", pib_percapta2);
+
+    printf("\n\nPressione ENTER para sair...");
+    getchar();
+    getchar();
 
     return 0;
 }
