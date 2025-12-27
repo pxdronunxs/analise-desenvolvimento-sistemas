d1e308b8539dd5714eaff3d93c5139e5741f4824
pxdronunxs <pn4334@gmail.com>
2025-12-27 17:54:01 -0300

    testes

---
+++ 1-periodo/exercicios/teste.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    int idade = 12;
+
+    if(idade<18) {
+        printf("Menor de idade\n");
+    } else if (idade>=18) {
+        printf("Maior de idade\n");
+    }
+}
