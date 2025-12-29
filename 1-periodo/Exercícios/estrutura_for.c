246edbe0710f3979ee061d8c2193b80a438b87eb
pxdronunxs <pn4334@gmail.com>
2025-12-29 11:26:46 -0300

    exemplos de estrutura for em C

--- 1-periodo/Exercícios/estrutura_for.c
+++ 1-periodo/Exercícios/estrutura_for.c
@@ -1,9 +1,47 @@
+// #include <stdio.h>
+
+// int main() {
+//     for (int numero = 1; numero <= 5; numero++) {
+//         printf("%d\n", numero);
+//     }
+
+//     return 0;
+// }
+
+//----------------------------------------------------------------------//
+
+// #include <stdio.h>
+ 
+// int main() {
+//     for (int i = 1; i <= 20; i++) {
+//         if (i % 2 == 0) {
+//             printf("%d\n", i);
+//         }
+//     }
+   
+//     return 0;
+// }
+
+//----------------------------------------------------------------------//
+
+// #include <stdio.h>
+ 
+// int main() {
+//     for (int i = 0; i <= 100; i += 10) {
+//         printf("%d\n", i);
+//     }
+   
+//     return 0;
+// }
+
+//----------------------------------------------------------------------//
+
 #include <stdio.h>
-
+ 
 int main() {
-    for (int numero = 1; numero <= 5; numero++) {
-        printf("%d\n", numero);
+    for (int i = 100; i >= 0; i -= 10) {
+        printf("%d\n", i);
     }
-
+   
     return 0;
 }
