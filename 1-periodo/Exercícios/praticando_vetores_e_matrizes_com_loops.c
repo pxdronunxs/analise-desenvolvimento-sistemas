1c17e313539e4768fbbf607c4f59ff500b5ed707
pxdronunxs <pn4334@gmail.com>
2026-01-07 22:02:19 -0300

    praticando vetores e matrizes com loops

---
+++ 1-periodo/Exercícios/praticando_vetores_e_matrizes_com_loops.c
@@ -0,0 +1,63 @@
+// #include <stdio.h>
+ 
+// int main() {
+//     int vetor[5]; // Declaração de um vetor de 5 inteiros
+ 
+//     // Inicialização do vetor usando uma estrutura de repetição for
+//     for (int i = 0; i < 5; i++) {
+//         vetor[i] = i * 2; // Atribuindo valores ao vetor
+//     }
+ 
+//     // Exibindo os elementos do vetor usando uma estrutura de repetição for
+//     for (int i = 0; i < 5; i++) {
+//         printf("vetor[%d] = %d\n", i, vetor[i]);
+//     }
+ 
+//     return 0;
+// }
+
+
+/*===================================================*/
+
+
+// #include <stdio.h>
+ 
+// int main() {
+//     int matriz[3][3]; // Declaração de uma matriz 3x3 de inteiros
+ 
+//     // Inicialização da matriz usando estruturas de repetição for aninhadas
+//     for (int i = 0; i < 3; i++) {
+//         for (int j = 0; j < 3; j++) {
+//             matriz[i][j] = i + j; // Atribuindo valores à matriz
+//         }
+//     }
+ 
+//     // Exibindo os elementos da matriz usando estruturas de repetição for aninhadas
+//     for (int i = 0; i < 3; i++) {
+//         for (int j = 0; j < 3; j++) {
+//             printf("matriz[%d][%d] = %d\n", i, j, matriz[i][j]);
+//         }
+//     }
+ 
+//     return 0;
+// }
+
+/*===================================================*/
+
+#include <stdio.h>
+
+int main() {
+    
+    int vetor[5] = {1, 2, 3, 4, 5};
+    int soma = 0;
+
+    // calculando a soma dos elementos do vetor
+    for (int i = 0; i < 5; i++) {
+        soma += vetor[i];
+
+    }
+
+    printf("A soma dos elementos do vetor é: %d\n", soma);
+
+    return 0;
+}
