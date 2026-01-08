de7dd582520b45cd3998605178b78f3f99904d15
pxdronunxs <pn4334@gmail.com>
2026-01-08 11:18:01 -0300

    praticando vetores e matrizes com loops

--- 1-periodo/Exercícios/praticando_vetores_e_matrizes_com_loops.c
+++ 1-periodo/Exercícios/praticando_vetores_e_matrizes_com_loops.c
@@ -44,20 +44,50 @@
 
 /*===================================================*/
 
+// #include <stdio.h>
+
+// int main() {
+    
+//     int vetor[5] = {1, 2, 3, 4, 5};
+//     int soma = 0;
+
+//     // calculando a soma dos elementos do vetor
+//     for (int i = 0; i < 5; i++) {
+//         soma += vetor[i];
+
+//     }
+
+//     printf("A soma dos elementos do vetor é: %d\n", soma);
+
+//     return 0;
+// }
+
+/*===================================================*/
+
+//soma de matrizes
+
 #include <stdio.h>
 
 int main() {
-    
-    int vetor[5] = {1, 2, 3, 4, 5};
-    int soma = 0;
+    int matriz1[2][2] = {{1, 2}, {3, 4}};
+    int matriz2[2][2] = {{3, 4}, {5, 6}};
+    int matrizSoma[2][2];
 
-    // calculando a soma dos elementos do vetor
-    for (int i = 0; i < 5; i++) {
-        soma += vetor[i];
-
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            matrizSoma[i][j] = matriz1[i][j] + matriz2[i][j];
+        }
     }
 
-    printf("A soma dos elementos do vetor é: %d\n", soma);
+    //exibir matriz resultante
+
+    for (int i = 0; i < 2; i++) {
+        for (int j = 0; j < 2; j++) {
+            printf("matrizSoma[%d][%d] = %d\n", i, j,
+            matrizSoma[i][j]);
+        }
+    }
+    
 
     return 0;
 }
